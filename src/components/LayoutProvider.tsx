f64832efbacdafccc4cf0779d2a32e77c047e1f1
C-W-D-Harshit <cwd.harshit911@gmail.com>
2024-12-30 12:10:17 +0000

    Add client-side rendering support to LayoutProvider component

--- src/components/LayoutProvider.tsx
+++ src/components/LayoutProvider.tsx
@@ -1,13 +1,20 @@
+"use client";
+
 import { AppSidebar } from "@/components/app-sidebar";
 
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import Header from "./Header";
+import { useEffect, useState } from "react";
 
 export default function LayoutProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
   return (
     <SidebarProvider>
       <AppSidebar />
