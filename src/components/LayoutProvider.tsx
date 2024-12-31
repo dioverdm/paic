b201fe0715c46306c314636e811d5daab37fddd0
C-W-D-Harshit <cwd.harshit911@gmail.com>
2024-12-31 11:26:10 +0000

    Add mobile optimization warning in LayoutProvider component

--- src/components/LayoutProvider.tsx
+++ src/components/LayoutProvider.tsx
@@ -16,12 +16,18 @@ export default function LayoutProvider({
 
   if (!mounted) return null;
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <Header />
-        <main className="flex-1 flex flex-col p-4 pt-0">{children}</main>
-      </SidebarInset>
-    </SidebarProvider>
+    <div>
+      <SidebarProvider className="hidden md:flex">
+        <AppSidebar />
+        <SidebarInset>
+          <Header />
+          <main className="flex-1 flex flex-col p-4 pt-0">{children}</main>
+        </SidebarInset>
+      </SidebarProvider>
+      <div className="fixed bottom-0 left-0 right-0 p-4 bg-red-500 text-white text-center md:hidden">
+        Sorry for the inconvenience, but this app is not yet optimized for
+        mobile devices.
+      </div>
+    </div>
   );
 }
