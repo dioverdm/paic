f93ff6ce703a936d753a2d162b2a8c90673ce641
C-W-D-Harshit <cwd.harshit911@gmail.com>
2025-01-15 06:03:49 +0000

    Refactor Next.js configuration and add experimental React compiler support; update package.json to include babel-plugin-react-compiler

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next/dist/server/config";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  experimental: {
+    reactCompiler: true,
   },
 };
 
